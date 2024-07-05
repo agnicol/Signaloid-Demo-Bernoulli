129c8f0e7a4b124d3a7a77881833e0373d1a1691
Andy Nicol <andy@redleadertech.com>
2024-07-05 11:00:38 +0100

    fix submodule linking

---
+++ src/common.c
@@ -0,0 +1,1 @@
+../submodules/Signaloid-Demo-CommonUtilityRoutines/common.c
--- src/config.mk
+++ src/config.mk
@@ -1,10 +1,9 @@
 # Lines starting with '#' are comments.
 
 # Explicitely specify which files to compile
-SOURCES = main.c
-SOURCES += utilities.c
-SOURCES += ../submodules/Signaloid-Demo-CommonUtilityRoutines/common.c
+SOURCES = $(wildcard *.c)
 
 # Options that get passed to the C compiler
+CFLAGS = -I./
 
-#CFLAGS = -DDEBUG
+#CFLAGS += -DDEBUG
